752f6b1e599396360d1f27ce290307f7c177a546
Jonathan.Souza <jonathan.souza@sicoob.com.br>
2024-04-12 11:13:16 -0300

    elemento de paginação

---
+++ src/app/shared/paginator/paginator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tgt-paginator',
+  standalone: true,
+  imports: [],
+  templateUrl: './paginator.component.html',
+  styleUrl: './paginator.component.scss'
+})
+export class PaginatorComponent {
+
+}
