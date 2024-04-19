99be62ac2f13c97079b15bcb7ebaa7815675af8e
Jonathan.Souza <jonathan.souza@sicoob.com.br>
2024-04-19 15:54:13 -0300

    criando component de view card

--- src/app/private/admin/pages/configurations/cartoes/cartoes.component.ts
+++ src/app/private/admin/pages/configurations/cartoes/cartoes.component.ts
@@ -3,13 +3,14 @@ import { ButtonComponent } from "../../../../../shared/button/button.component";
 import { DividerComponent } from "../../../../../shared/divider/divider.component";
 import { SidebarService } from '../../../../../shared/sidebar/sidebar.service';
 import { SidebarNovoCartaoComponent } from './components/sidebar-novo-cartao/sidebar-novo-cartao.component';
+import { CreditCardViewComponent } from "../../../../../shared/credit-card-view/credit-card-view.component";
 
 @Component({
     selector: 'cartoes',
     standalone: true,
     templateUrl: './cartoes.component.html',
     styleUrl: './cartoes.component.scss',
-    imports: [ButtonComponent, DividerComponent]
+    imports: [ButtonComponent, DividerComponent, CreditCardViewComponent]
 })
 export class CartoesComponent {
 
