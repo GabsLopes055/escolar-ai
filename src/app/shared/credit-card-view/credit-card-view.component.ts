98db2a97b80385eb7c3cfc120f1de8098258f95d
Jonathan Lauro <jonathanlauro.dev@gmail.com>
2024-04-20 18:46:27 -0300

    ajustando os cartões

--- src/app/shared/credit-card-view/credit-card-view.component.ts
+++ src/app/shared/credit-card-view/credit-card-view.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CardNumberPipe } from "../pipes/card-number/card-number.pipe";
 import { MouthCardPipe } from "../pipes/mouth-card/mouth-card.pipe";
+import { Cartao } from '../../models/cartao.interface';
 
 @Component({
     selector: 'tgt-credit-card-view',
@@ -11,4 +12,10 @@ import { MouthCardPipe } from "../pipes/mouth-card/mouth-card.pipe";
 })
 export class CreditCardViewComponent {
 
+    @Input() cartao: Cartao = {
+        cardNumber: '5445 8211 7845 1234',
+        mouthValidate: '20/10/2025',
+        nameCard: 'Jonathan L M Souza',
+        cvv: '306'
+    }
 }
