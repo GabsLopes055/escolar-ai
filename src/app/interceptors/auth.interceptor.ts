4a3acacc425934a40901e193cdb891e0421298c2
Jonathan.Souza <jonathan.souza@sicoob.com.br>
2024-05-24 09:35:54 -0300

    melhoras no sistema

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,13 +1,32 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import {inject} from "@angular/core";
+import {JwtServiceService} from "../shared/services/token/jwt-service.service";
+import {Router} from "@angular/router";
+import {MenuService} from "../shared/menu/menu.service";
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const jwtService = inject(JwtServiceService);
+  const router = inject(Router);
+  const menuService = inject(MenuService);
+
   const token = window.sessionStorage.getItem('token');
+
+
   if (token) {
-    req = req.clone({
-      setHeaders: {
-        Authorization: 'Bearer ' + token,
-      }
-    });
+    const isValid = jwtService.isTokenValid(token as string);
+    if (isValid) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + token,
+        }
+      });
+    } else {
+      window.sessionStorage.removeItem('token');
+      router.navigate(['login']);
+      menuService._menu.next([]);
+    }
+
   }
   return next(req);
 };
--- src/app/private/admin/pages/configurations/central-custo/components/sidebar-nova-central-custo/sidebar-nova-central-custo.component.ts
+++ src/app/private/admin/pages/configurations/central-custo/components/sidebar-nova-central-custo/sidebar-nova-central-custo.component.ts
@@ -5,21 +5,39 @@ import { SidebarComponent } from "../../../../../../../shared/sidebar/sidebar.co
 import { InputComponent } from "../../../../../../../shared/input/input.component";
 import { ToggleComponent } from "../../../../../../../shared/toggle/toggle.component";
 import { ButtonComponent } from "../../../../../../../shared/button/button.component";
+import {
+  OptionSelect,
+  SelectComponent
+} from "../../../../../../../shared/select/select.component";
+import {FormControl} from "@angular/forms";
 
 @Component({
     selector: 'app-sidebar-nova-central-custo',
     standalone: true,
     templateUrl: './sidebar-nova-central-custo.component.html',
     styleUrl: './sidebar-nova-central-custo.component.scss',
-    imports: [SidebarComponent, InputComponent, ToggleComponent, ButtonComponent]
+    imports: [SidebarComponent, InputComponent, ToggleComponent, ButtonComponent, SelectComponent]
 })
 export class SidebarNovaCentralCustoComponent {
+
+  options: OptionSelect[] = [
+    {label: 'Masculino', value: 'MASCULINO'},
+    {label: 'Feminino', value: 'FEMININO'},
+
+  ];
+
+  tetoOrcamentario = false;
     constructor(
         private sidebarService: SidebarService
     ) {}
 
     close() {
         this.sidebarService.closeSide();
+    }
+
+    tetoChange(data: any) {
+      this.tetoOrcamentario = data;
+      console.log(data)
     }
 
 }
