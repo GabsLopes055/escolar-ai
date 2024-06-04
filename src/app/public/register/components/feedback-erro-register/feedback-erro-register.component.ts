b0521219f0f68f2bd1c969e2bc7a58380fff311a
Jonathan.Souza <jonathan.souza@sicoob.com.br>
2024-06-04 11:18:15 -0300

    chamados

---
+++ src/app/public/register/components/feedback-erro-register/feedback-erro-register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'feedback-erro-register',
+  standalone: true,
+  imports: [],
+  templateUrl: './feedback-erro-register.component.html',
+  styleUrl: './feedback-erro-register.component.scss'
+})
+export class FeedbackErroRegisterComponent {
+
+}
